7ec8c30bee8c171d1accb9d0da92973b70aa6ed9
Stephen Dickinson <stephencottontail@me.com>
2019-03-18 16:56:29 -0600

    Style the editor view

--- index.js
+++ index.js
@@ -1,30 +1,70 @@
 import './src/scss/block.scss'
 import './src/scss/editor.scss'
 import { registerBlockType } from '@wordpress/blocks'
-import { SVG, Path } from '@wordpress/components'
+import { PlainText } from '@wordpress/editor'
+import { SVG, Path, TextControl, TextareaControl } from '@wordpress/components'
+import { withState } from '@wordpress/compose'
 import URLInputButton from './src/vendor/url-input/button'
+
+const BannerTitle = withState( {
+    title: '',
+} )( ( { title, setState } ) => ( 
+    <TextControl
+    label="Banner Title"
+    value={ title }
+    onChange={ ( title ) => setState( { title } ) }
+    />
+) )
+const BannerContent = withState( {
+    content: '',
+} )( ( { content, setState } ) => ( 
+    <TextareaControl
+    label="Banner Content"
+    value={ content }
+    onChange={ ( content ) => setState( { content } ) }
+    />
+) )
 
 registerBlockType( 'limeguten/banner', {
     title: 'Banner',
     icon: <SVG viewBox="0 0 32 32" version="1" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" strokeLinejoin="round" strokeMiterlimit="1"><Path d="M5 13h22v7H5zM1 14h4v7H1l2-3-2-4M31 21h-4v-7h4l-2 4 2 3" /></SVG>,
     category: 'limecuda',
     attributes: {
-        url: {
+        title: {
             type: 'string',
+            default: ''
+        },
+        content: {
+            type: 'string',
+            default: ''
+        },
+        url: {
+            type: 'string'
         },
         text: {
             type: 'string'
         }
     },
     edit( { className, attributes, setAttributes } ) {
-        const { url, text } = attributes
-
+        const { title, content, url, text } = attributes
+        const changeUrl = ( url, post ) => setAttributes( { url, text: ( post && post.title ) || text } )
+        
         return (
             <div className={ className }>
-                <URLInputButton
-                url={ url }
-                onChange={ ( url, post ) => setAttributes( { url, text: (post && post.title) || 'Click here' } ) }
-                />
+                <BannerTitle />
+                <BannerContent />
+                <div className={ 'banner-cta' }>
+                    <label>{ 'Banner CTA' }</label>
+                    <PlainText
+                    value={ text }
+                    onChange={ ( text ) => setAttributes( text ) }
+                    placeholder={ 'Click here...' }
+                    />
+                    <URLInputButton
+                    url={ url }
+                    onChange={ changeUrl }
+                    />
+                </div>
             </div>
         )
     },
